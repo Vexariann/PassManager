4503ca3a4d9da18cdf2a6dfc00fc7bac8ffa10c6
Vexarian <julianpasker@gmail.com>
2023-01-26 13:36:21 +0100

    minor change

--- PassManager.Interfaces/Models/User.cs
+++ PassManager.Interfaces/Models/User.cs
@@ -6,7 +6,5 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string ProfilePicture { get; set; }
-        public Options Options { get; set; }
-        public int OptionsID { get; set; }
     }
 }
